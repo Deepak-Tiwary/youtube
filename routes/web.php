0c17493398d6ac20bf507e7724586dc7e6159ecf
Deepak Kumar <deepraj.tiwary@gmail.com>
2020-08-23 18:39:31 +0100

    Youtube Live Video Search, Final Commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tubeSearch');
 });
+Route::post('/search', 'getYoutubeVideos@search_results');
